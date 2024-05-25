5f280b24c4fb3c8819d03d2a19664dc56eb873dc
Artem Melnikov <94278422+Lolanimus@users.noreply.github.com>
2024-05-25 19:05:46 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Template1
+A template for projects that use Webpack, ESLint, Prettier and Babel.
+
+IMPORTANT: Type 'npm install' in CLI to install all packages. Otherwise nothing will work.
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Home",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+};
